bb4a31bff7e93c42532ecf6af38cd4efe647b44c
elisaul batista <elisaulbatista@pursuit.org>
2023-11-16 12:18:48 -0500

    creater the folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  );
-};
+    
+      <h1>app</h1>
+   
+  )
+}
 
 export default App
