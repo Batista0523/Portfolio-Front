025772f82b6793c20d8a437f9a50161b8b4d8cf9
elisaul batista <elisaulbatista@pursuit.org>
2023-11-18 18:35:58 -0500

    working in the icon circle need to update the img size

--- src/constants/images.js
+++ src/constants/images.js
@@ -8,16 +8,16 @@ import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
-import javascript from '../assets/javascript.PNG'
+import javascript from '../assets/Javascript.png'
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
 import python from '../assets/python.png';
-import react from '../assets/react.PNG'
+import react from '../assets/react.png'
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
-import trello from '../assets/trello.png'
+import trello from '../assets/trello logo.png'
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
@@ -26,7 +26,7 @@ import about04 from '../assets/about04.png';
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
-import canva from '../assets/canva.png';
+import canva from '../assets/Canva logo.png'
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
